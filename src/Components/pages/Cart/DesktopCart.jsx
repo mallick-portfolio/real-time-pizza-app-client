f07ed3fce59c7927512e7e8e0c2faa7daaaac04b
mallick-portfolioE <tamal1714019@gmail.com>
2022-06-18 00:47:26 +0600

    added add to cart product

--- src/Components/pages/Cart/DesktopCart.jsx
+++ src/Components/pages/Cart/DesktopCart.jsx
@@ -15,49 +15,56 @@ const DesktopCart = ({ userOrders }) => {
         <hr className="border-1 my-2 border-primary" />
         {userOrders.map((order) => (
           <>
-            <div
-              key={order?._id}
-              className="flex justify-between py-4 items-center"
-            >
-              <div className="flex  items-center ">
-                <img className="w-28 mr-4" src={order?.image} alt="" />
-                <div>
-                  <h4 className="text-2xl">{order?.name}</h4>
-                  <h5 className="text-xl">{order?.size}</h5>
+            <div>
+              <div
+                key={order?._id}
+                className="flex justify-between py-4 items-center"
+              >
+                <div className="flex  items-center ">
+                  <img className="w-28 mr-4" src={order?.image} alt="" />
+                  <div>
+                    <h4 className="text-2xl">{order?.name}</h4>
+                    <h5 className="text-xl">{order?.size}</h5>
+                  </div>
+                </div>
+                <div className="flex items-center ">
+                  <button className="text-xl  cursor-pointer px-4 py-1   border rounded-full text-white hover:bg-primaryhover bg-primary smooth text-center my-4">
+                    <BsPlusLg />
+                  </button>
+                  <input
+                    readOnly
+                    type="text"
+                    value={4}
+                    className="focus:outline-none w-1/6 text-center"
+                  />
+                  <button className="text-xl  cursor-pointer px-4 py-1   border rounded-full text-white hover:bg-primaryhover bg-primary smooth text-center my-4">
+                    <FaMinus />
+                  </button>
+                </div>
+                <div className="text-xl">${order?.price}</div>
+                <div className="text-xl">
+                  <button className="text-sm px-4 py-2  cursor-pointer border rounded-full text-white hover:bg-primaryhover bg-primary smooth text-center">
+                    Remove
+                  </button>
                 </div>
               </div>
-              <div className="flex items-center ">
-                <button className="text-xl  cursor-pointer px-4 py-1   border rounded-full text-white hover:bg-primaryhover bg-primary smooth text-center my-4">
-                  <BsPlusLg />
-                </button>
-                <input
-                  readOnly
-                  type="text"
-                  value={4}
-                  className="focus:outline-none w-1/6 text-center"
-                />
-                <button className="text-xl  cursor-pointer px-4 py-1   border rounded-full text-white hover:bg-primaryhover bg-primary smooth text-center my-4">
-                  <FaMinus />
-                </button>
-              </div>
-              <div className="text-xl">${order?.price}</div>
-              <div className="text-xl">
-                <button className="text-sm px-4 py-2  cursor-pointer border rounded-full text-white hover:bg-primaryhover bg-primary smooth text-center">
-                  Remove
-                </button>
-              </div>
             </div>
-            <div className="text-right">hello world</div>
+            <div className="text-right">
+              Total{" "}
+              {userOrders.reduce(function (accumulator, item) {
+                return accumulator + item.price;
+              }, 0)}
+            </div>
           </>
         ))}
-        {!userOrders && (
-          <div>
-            {console.log(userOrders)}
-            <h1>No Order Found</h1>
-            <img className="w-1/2 mx-auto" src={emptycart} alt="" />
-          </div>
-        )}
       </div>
+      {!userOrders && (
+        <div>
+          {console.log(userOrders)}
+          <h1>No Order Found</h1>
+          <img className="w-1/2 mx-auto" src={emptycart} alt="" />
+        </div>
+      )}
     </section>
   );
 };
